319c05e3163c87755b4bc95177db12ffcf7e82b8
Soso0303 <ismailechchafiy56@gmail.com>
2024-04-17 12:31:06 +0100

    strecture for project

---
+++ frontOffice/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
